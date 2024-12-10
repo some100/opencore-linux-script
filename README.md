293956d797b53270d3827cc618d4277cd5913e05
someone <40119497+some100@users.noreply.github.com>
2024-12-10 16:04:49 +0000

    Update README + Slightly change config + Change install script

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # opencore-linux-script
 Attempts to automate installation of OpenCore solely for booting Linux systems
 
+## Disclaimer
+This script probably shouldn't be seriously used yet. I've only been able to verify it working inside of a VM. If you do decide to test this, install it to your USB drive, not your EFI partition. I am also not sure if this will work with older firmwares (pre-2017).
+
+**THIS WILL NOT PRODUCE A USABLE EFI FOR MACOS!** For hackintoshing, use the excellent [OpenCore Install Guide](https://dortania.github.io/OpenCore-Install-Guide/) instead of this tool.
+
 ## What is OpenCore?
-OpenCore is a boot manager that was mainly created for the purposes of hackintoshing, or installing macOS on non-Apple hardware. However, it can also boot other operating systems such as Linux and Windows. This script mainly focuses on the former, and creates a configuration that, while useless for hackintoshing, is enough for booting Linux.
+OpenCore is a boot manager that was mainly created for the purposes of hackintoshing, or installing macOS on non-Apple hardware. However, it can also boot other operating systems such as Linux and Windows. This script creates a configuration that, while useless for hackintoshing, is enough for booting Linux.
 
 ## Why use OpenCore over something like GRUB or systemd-boot?
-Well, OpenCore generally is much faster than GRUB. It's a much smaller binary as well. The entire boot manager, with drivers included, has a lower disk usage than just grubx64.efi. OpenCore has many security features, such as Vaulting, picker password protection, and trusted loading with UEFI Secure Boot. OpenCore's development also follows best practices for security, such as fuzzing and formal review. OpenCore also generally looks nicer with a macOS style boot picker.
+Well, OpenCore generally is much faster than GRUB. It's more lightweight as well. The entire boot manager, with drivers included, has a lower disk usage than just grubx64.efi. OpenCore has security features like vaulting, password protection, and integration with UEFI Secure Boot. OpenCore's development also follows best practices for security, such as fuzzing and formal review. OpenCore also has a macOS-style boot picker, which looks (in my opinion) much nicer than systemd-boot and (default) GRUB. 
+
+## Configuration
+OpenCore uses the config.plist to customize behavior inside of the boot manager. On the surface, it's much more complicated than configuring systemd-boot and GRUB. However, many parts of the config.plist can be ignored since we're only focusing on booting Linux. Generally, the [Configuration](https://dortania.github.io/docs/latest/Configuration.html) can help you find your way through the configuration. To increase security, you can set up a [picker password](https://dortania.github.io/OpenCore-Post-Install/universal/security/password.html), [ScanPolicy](https://dortania.github.io/OpenCore-Post-Install/universal/security/scanpolicy.html), and [vaulting](https://dortania.github.io/OpenCore-Post-Install/universal/security/vault.html).
--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 OCVER=$(curl --silent -qI https://github.com/acidanthera/OpenCorePkg/releases/latest | awk -F '/' '/^location/ {print  substr($NF, 1, length($NF)-1)}');
 
-echo 'Welcome!'
-echo 'This script will create an EFI for OpenCore that can be used exclusively for Linux and Windows booting. It cannot be used for hackintoshing or booting macOS. For that purpose, the official install guide at https://dortania.github.io/OpenCore-Install-Guide/ should be used instead.'
-printf 'Use DEBUG version of OpenCore? (y/N) '
+echo "Welcome!"
+echo "This script will create an EFI for OpenCore that can be used exclusively for Linux and Windows booting. It cannot be used for hackintoshing or booting macOS. For that purpose, the official install guide at https://dortania.github.io/OpenCore-Install-Guide/ should be used instead."
+printf '%s' "Use DEBUG version of OpenCore? (y/N) "
 read answer
 
 if [ "$answer" != "${answer#[Yy]}" ]; then
@@ -18,7 +18,7 @@ echo 'Downloading the latest release of OpenCore...'
 mkdir OpenCore
 wget -q https://github.com/acidanthera/OpenCorePkg/releases/download/$OCVER/OpenCore-$OCVER-$BUILD.zip
 unzip -qq OpenCore-$OCVER-$BUILD.zip -d OpenCore
-printf 'Is your EFI architecture IA32, or 32 bit? (If in doubt, select no) (y/N) '
+printf '%s' "Is your EFI architecture IA32, or 32 bit? (If in doubt, select no) (y/N) "
 read answer
 
 if [ "$answer" != "${answer#[Yy]}" ]; then
@@ -26,33 +26,43 @@ if [ "$answer" != "${answer#[Yy]}" ]; then
 else
 	ARCH=X64
 fi
-echo 'Copying config.plist...'
-cp configs/config.plist OpenCore/$ARCH/EFI/OC/config.plist
-echo 'Removing unneeded drivers...'
-find OpenCore/$ARCH/EFI/OC/Drivers/* ! -name 'OpenRuntime.efi' ! -name 'OpenLinuxBoot.efi' ! -name 'OpenCanopy.efi' ! -name 'Ext4Dxe.efi' -exec rm -f {} +
-echo 'Removing unneeded tools...'
-rm -f OpenCore/$ARCH/EFI/OC/Tools/*
-echo 'Setting up a theme for OpenCanopy'
-cp -r OcBinaryData/Resources/ OpenCore/$ARCH/EFI/OC/
-printf 'What filesystem is your /boot partition on? (ext4, btrfs, etc.): '
+OCPATH=OpenCore/$ARCH/EFI/OC
+
+echo "Removing unneeded drivers..."
+find $OCPATH/Drivers/* ! -name 'OpenRuntime.efi' ! -name 'OpenLinuxBoot.efi' ! -name 'OpenCanopy.efi' ! -name 'Ext4Dxe.efi' -exec rm -f {} +
+echo "Removing unneeded tools..."
+rm -f $OCPATH/Tools/*
+echo "Setting up a theme for OpenCanopy"
+cp -r OcBinaryData/Resources/ $OCPATH/
+printf '%s' "What filesystem is your /boot partition on? (ext4, btrfs, etc.): "
 read answer
 answer=$(echo "$answer" | awk '{print tolower($0)}')
 
-if [ "$answer" != "ext4" ]; then
+if [ "$answer" != "ext4" ] && [ "$answer" != "" ]; then
 	echo 'Getting filesystem driver...'
-	rm OpenCore/$ARCH/EFI/OC/Drivers/Ext4Dxe.efi
+	rm $OCPATH/Drivers/Ext4Dxe.efi
 	wget -q https://github.com/pbatard/EfiFs/releases/latest/download/$answer\_$ARCH.efi || { 
-	echo 'Failed to get filesystem driver! Maybe it is not supported yet. If you think it is supported, try opening an issue.'; exit 
+	echo "Failed to get filesystem driver! Maybe it is not supported yet. If you think it is supported, try opening an issue."; exit 
 	}
-	echo 'Moving filesystem driver...'
-	mv $answer\_$ARCH.efi OpenCore/$ARCH/EFI/OC/Drivers/
-	echo 'Copying changes to config.plist...'
-	sed -i 's/<string>Ext4Dxe.efi<\/string>/<string>$answer<\/string>/' OpenCore/$ARCH/EFI/OC/config.plist
+	echo "Moving filesystem driver..."
+	mv $answer\_$ARCH.efi $OCPATH/Drivers/
+	echo "Copying changes to config.plist..."
+	sed "s/<string>Ext4Dxe.efi<\/string>/<string>$answer\_$ARCH.efi<\/string>/" configs/config.plist 1> $OCPATH/config.plist
+	echo "Using $answer as filesystem"
+else
+	echo "Using ext4 as filesystem"
 fi
 
-printf 'Where should OpenCore be installed? (example: /boot/efi, /efi, your USB drive, etc.) '
+printf '%s' "Where should OpenCore be installed? (example: /boot/efi, /efi, your USB drive, etc.) "
 read answer
+if [ "$answer" = "" ]; then
+	echo 'No directory specified!'
+	exit
+elif [ ! -d "$answer" ]; then
+	echo 'Directory does not exist!'
+	exit
+fi
 
-cp -r OpenCore/$ARCH/EFI/* $answer/EFI/
+cp -r OpenCore/$ARCH/* $answer/ || echo 'Failed to copy OpenCore to selected directory!'; exit
 
-echo 'Done!'
+echo 'Done! Please reboot your machine.'
